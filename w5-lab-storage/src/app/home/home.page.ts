85933e00e57ce0c93f00909a69c3aa7677e54695
AaronCronnelly <101333389+AaronCronnelly@users.noreply.github.com>
2024-10-18 09:13:24 +0000

    Using ionic storage

--- w5-lab-storage/src/app/home/home.page.ts
+++ w5-lab-storage/src/app/home/home.page.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Storage } from '@ionic/storage-angular';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
-  constructor() {}
+  key: string = '';
+  value: string = '';
+  output: string = '';
+
+  constructor(private storage: Storage) {
+    storage.create();
+  }
+
+  async setItem(){
+    try{
+      await this.storage.set(this.key, this.value);
+      this.output = 'Set ${this.key}: ${this.value}';
+    } catch(e){
+      console.error('Error setting item', e);
+      this.output = `Error setting item: ${e}`;
+    }
+  }
+
+  async getItem() {
+    try{
+      const value = await this.storage.get(this.key);
+      this.output = `Got ${this.key}: ${value}`;
+    }catch(e){
+      console.error('Error getting item', e);
+      this.output = `Error getting item: ${e}`;
+    }
+  }
+
+
 }
